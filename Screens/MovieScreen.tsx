b5fcda5869cbeff364dd3069f3a0bf2fc9844160
Ahmed Ramy <ahmedramy.e404@gmail.com>
2022-05-24 22:39:46 +0200

    feat: :sparkles: Added lazy loading

--- Screens/MovieScreen.tsx
+++ Screens/MovieScreen.tsx
@@ -1,19 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MovieList from '../components/MovieList';
+import { StyleSheet, Text, FlatList, ActivityIndicator, SafeAreaView} from 'react-native';
 import { useConfigurationQuery } from '../framework/basic-rest/get-configuration';
 import { useMoviesQuery } from '../framework/basic-rest/Movies/get-movies';
 import ErrorScreen from './ErrorScreen';
 import LoadingScreen from './LoadingScreen';
+import { Movie } from '../components/Movie';
 
 export default function MovieScreen() {
-    const {data: movieData, isLoading: isLoadingMovies, error: errorMovies} = useMoviesQuery();
-    const {data: configurationData, isLoading: isLoadingConfiguration, error: errorConfiguration} = useConfigurationQuery();
-    if(isLoadingMovies || isLoadingConfiguration)
-        return <LoadingScreen />
-    if(errorMovies || errorConfiguration)
-        return <ErrorScreen />
+  const {data: movieData, isLoading: isLoadingMovies, error: errorMovies, hasNextPage, fetchNextPage, isFetching} = useMoviesQuery();
+  const {data: configurationData, isLoading: isLoadingConfiguration, error: errorConfiguration} = useConfigurationQuery();
+
+  const renderItem: (item: any) => any = ({item}) => {
+    return <Movie movie={item} configuration = {configurationData} />;
+  }
+  const keyExtractor: (item: any) => any = (item) => {
+    return item.id
+  }
+
+  const loadMore = () => {
+    if(hasNextPage)
+      fetchNextPage();
+  }
+
+  if(isLoadingMovies || isLoadingConfiguration)
+      return <LoadingScreen />
+  if(errorMovies || errorConfiguration)
+      return <ErrorScreen />
+
   return (
-    <MovieList movies={movieData.results} configuration = {configurationData} />
+    <SafeAreaView style = {styles.container}>
+      <FlatList onEndReached={loadMore} contentContainerStyle = {{flexGrow: 1}} data = {movieData!.pages.map((page: any) =>  page).flat()} renderItem = {renderItem} keyExtractor={keyExtractor}  onEndReachedThreshold = {0.2} />
+      {isFetching ? <ActivityIndicator size={'small'} color = "#999999" style={{marginBottom: "5%"}}/> : null}
+      {!hasNextPage ? <Text>No more movies to fetch</Text> : null}
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- framework/basic-rest/Movies/get-movies.tsx
+++ framework/basic-rest/Movies/get-movies.tsx
@@ -1,13 +1,17 @@
 // import { API_ENDPOINTS } from "@framework/utils/api-endpoints";
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useQuery, useInfiniteQuery } from "react-query";
 
-export const fetchMovies = async () => {
-	const { data } = await axios.get(`http://api.themoviedb.org/3/discover/movie?api_key=acea91d2bff1c53e6604e4985b6989e2&page=1`);
-	return data;
+export const fetchMovies = async ({pageParam = 1}) => {
+	const { data } = await axios.get(`http://api.themoviedb.org/3/discover/movie?api_key=acea91d2bff1c53e6604e4985b6989e2&page=${pageParam}`);
+	return data.results;
 };
 export const useMoviesQuery = () => {
-	return useQuery<any, Error>("Get Movies", () =>
-		fetchMovies()
-	);
+	return useInfiniteQuery<any, Error>(
+	["Get Movies"],
+	 fetchMovies, {
+		getNextPageParam: (_lastPage, pages) => {
+			return pages.length + 1;
+		}
+	});
 };
